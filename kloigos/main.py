df6082c5157012088dd0bdd430ba2dec9235356e
Fabio Ghirardello <fabio@cockroachlabs.com>
2025-12-20 15:26:42 -0500

    ..

--- kloigos/main.py
+++ kloigos/main.py
@@ -1,85 +1,139 @@
 import datetime as dt
-import sys
-import threading
+import json
+import sqlite3
 from typing import Any
 
-import yaml
-from fastapi import FastAPI, HTTPException, Request, status
+from fastapi import BackgroundTasks, FastAPI, HTTPException, Request, status
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+from util import MyRunner
 
 app = FastAPI(title="Κλοηγός")
 
-INVENTORY_FILE = "inventory.yaml"
 RANGE_SIZE = 200
 CPU_PORTS_MAP = {f"{k}": 2000 + i * RANGE_SIZE for i, k in enumerate(range(256))}
 
-# Use a lock to prevent race conditions when multiple Ansible playbooks
-# try to allocate hosts at the exact same time.
-inventory_lock = threading.Lock()
+SQLITE = "kloigos.sqlite"
 
 
-class HostDetails(BaseModel):
-    ansible_host: str
+class ComputeUnit(BaseModel):
+    compute_id: str
     hostname: str
     ip: str
+    cpu_count: int
     cpu_range: str
-    cpu_list: str
-    ports_range: str | None
+    region: str
+    zone: str
     status: str
     started_at: dt.datetime | None
     tags: dict[str, Any] | None
-    labels: list[str] | None
 
 
-class ProvisionRequest(BaseModel):
-    name: str
+class ComputeUnitResponse(ComputeUnit):
+    cpu_list: str
+    ports_range: str | None
 
 
-class DeprovisionRequest(BaseModel):
-    hostname: str
+class ComputeUnitRequest(BaseModel):
+    cpu_count: int
+    region: str
+    zone: str
+    tags: str
 
 
 @app.post(
     "/provision",
 )
 async def provision_resources(
-    pr: ProvisionRequest,
-) -> HostDetails:
+    req: ComputeUnitRequest,
+) -> ComputeUnitResponse:
 
-    result = manage_inventory_file("provision", pr.name)
+    cu_list = get_compute_units(
+        region=req.region,
+        zone=req.zone,
+        status="free",
+        cpu_count=req.cpu_count,
+        limit=1,
+    )
 
-    if isinstance(result, HostDetails):
-        return result
+    if cu_list is not None:
+        cu = cu_list[0]
+    else:
+        return HTTPException(status.HTTP_416_RANGE_NOT_SATISFIABLE)
+    
+    cpu_list = cpu_range_to_list(cu.cpu_range)
 
-    raise HTTPException(
-        status_code=status.HTTP_428_PRECONDITION_REQUIRED, detail="no host available"
+    s = CPU_PORTS_MAP[cpu_list[-1]]
+    ports_range = f"{s}-{s + RANGE_SIZE}"
+
+    with sqlite3.connect(SQLITE) as conn:
+        cur = conn.cursor()
+        cur.execute(
+            """
+            UPDATE compute_units
+            SET status='allocated'
+                tags = ?
+            WHERE compute_id = ?
+            """,
+            (json.dumps(req.tags), cu.compute_id),
+        )
+
+    return ComputeUnitResponse(
+        cpu_list=cpu_list,
+        ports_range=ports_range,
+        **cu.model_dump(),
     )
 
 
-@app.post(
-    "/deprovision",
+@app.delete(
+    "/deprovision/{compute_id}",
 )
 async def deprovision_resources(
-    dr: DeprovisionRequest,
-):
+    compute_id: str,
+    bg_task: BackgroundTasks,
+) -> None:
 
-    result = manage_inventory_file("deprovision", dr.hostname)
+    with sqlite3.connect(SQLITE) as conn:
+        cur = conn.cursor()
+        cur.execute(
+            """
+            UPDATE compute_units
+            SET status='terminating'
+            WHERE compute_id = ?
+            """,
+            (),
+        )
 
-    if result == "OK":
-        return
+    bg_task.add_task(clean_up_compute_unit, compute_id)
 
-    raise HTTPException(
-        status_code=status.HTTP_428_PRECONDITION_REQUIRED, detail=result
-    )
+
+def clean_up_compute_unit(compute_id: str) -> None:
+    job_status = MyRunner().launch_runner("clean_up.yaml")
+
+    status = "free" if job_status == "successful" else "unavailable"
+
+    with sqlite3.connect(SQLITE) as conn:
+        cur = conn.cursor()
+        cur.execute(
+            """
+            UPDATE compute_units
+            SET status = ?
+            WHERE compute_id = ?
+            """,
+            (status, compute_id),
+        )
 
 
 @app.get("/servers")
 async def list_servers(
+    compute_id: str | None = None,
+    region: str | None = None,
+    zone: str | None = None,
+    cpu_count: int | None = None,
     deployment_id: str | None = None,
     status: str | None = None,
-) -> list[HostDetails]:
+) -> list[ComputeUnitResponse]:
     """
     Returns a list of all servers.
     Optionally filter the results by 'deployment_id' or 'status' query parameters.
@@ -90,101 +144,32 @@ async def list_servers(
     - /servers?status=free
     """
 
-    filtered_hosts = []
-    inventory_raw = {}
+    cu = get_compute_units(
+        compute_id,
+        region,
+        zone,
+        cpu_count,
+        deployment_id,
+        status,
+    )
 
-    with open(INVENTORY_FILE, "r") as f:
-        inventory_raw: dict[str, dict] = yaml.safe_load(f)
+    inventory: list[ComputeUnitResponse] = []
 
-        inventory: list[HostDetails] = []
-        for k, v in inventory_raw.items():
-            cpu_list = cpu_range_to_list(v.get("cpu_range"))
+    for x in cu:
+        cpu_list = cpu_range_to_list(x.cpu_range)
 
-            s = CPU_PORTS_MAP[cpu_list[-1]]
-            ports_range = f"{s}-{s + RANGE_SIZE}"
+        s = CPU_PORTS_MAP[cpu_list[-1]]
+        ports_range = f"{s}-{s + RANGE_SIZE}"
 
-            inventory.append(
-                HostDetails(
-                    ansible_host=k,
-                    cpu_list=cpu_list,
-                    ports_range=ports_range,
-                    **v,
-                )
+        inventory.append(
+            ComputeUnitResponse(
+                cpu_list=cpu_list,
+                ports_range=ports_range,
+                **x.model_dump(),
             )
-
-    for x in inventory:
-
-        # Check deployment_id filter (if provided)
-        deployment_id_match = (deployment_id is None) or (
-            x.tags.get("deployment_id", "") == deployment_id
         )
 
-        # Check status filter (if provided)
-        status_match = (status is None) or (x.status == status)
-
-        # If both matches are true, include the host
-        if deployment_id_match and status_match:
-            # We explicitly create the AllocatedHost Pydantic object
-            filtered_hosts.append(x)
-
-    return filtered_hosts
-
-
-def manage_inventory_file(action: str, item: str):
-
-    with inventory_lock:
-        try:
-            # 1. Load Inventory
-            with open(INVENTORY_FILE, "r") as f:
-                inventory_raw: dict[str, dict] = yaml.safe_load(f)
-        except FileNotFoundError:
-            print(
-                f"Error: Inventory file '{INVENTORY_FILE}' not found.", file=sys.stderr
-            )
-            return {"status": "error", "message": f"Inventory file missing."}
-
-        inventory: list[HostDetails] = [
-            HostDetails(ansible_host=k, **v) for k, v in inventory_raw.items()
-        ]
-
-        if action == "provision":
-            # Find the first free host
-            available_host_name = None
-            for h in inventory:
-                if h.status == "free":
-                    available_host_name = h
-
-                    h.status = "running"
-                    h.deployment_id = item
-
-                    with open(INVENTORY_FILE, "w") as f:
-                        yaml.dump(
-                            [x.model_dump() for x in inventory],
-                            f,
-                            default_flow_style=False,
-                        )
-
-                    return available_host_name
-
-            return {"status": "error", "message": "No free hosts available"}
-
-        elif action == "deprovision":
-            for h in inventory:
-                if h.hostname == item:
-
-                    h.status = "free"
-                    h.deployment_id = None
-
-                    with open(INVENTORY_FILE, "w") as f:
-                        yaml.dump(
-                            [x.model_dump() for x in inventory],
-                            f,
-                            default_flow_style=False,
-                        )
-
-                    return "OK"
-
-            return "Host not found"
+    return inventory
 
 
 def cpu_range_to_list(s: str):
@@ -192,7 +177,7 @@ def cpu_range_to_list(s: str):
     Convert from cpu_short syntax to a comma separated list
 
     Examples:
-    
+
     "0-7:2" -> "0,2,4,6"
 
     "1-7:2" -> "1,3,5,7"
@@ -219,6 +204,77 @@ def cpu_range_to_list(s: str):
     return ",".join([str(x) for x in list(range(start, end + 1, step))])
 
 
+def get_compute_units(
+    compute_id: str = None,
+    region: str = None,
+    zone: str = None,
+    cpu_count: int = None,
+    deployment_id: str = None,
+    status: str = None,
+    limit: int = None,
+) -> list[ComputeUnit]:
+
+    conditions = []
+    params = []
+
+    if compute_id is not None:
+        conditions.append("compute_id = ?")
+        params.append(compute_id)
+
+    if region is not None:
+        conditions.append("region = ?")
+        params.append(region)
+
+    if zone is not None:
+        conditions.append("zone = ?")
+        params.append(zone)
+
+    if cpu_count is not None:
+        conditions.append("cpu_count = ?")
+        params.append(cpu_count)
+
+    if deployment_id is not None:
+        conditions.append("json_extract(tags, '$.deployment_id') = ?")
+        params.append(status)
+
+    if status is not None:
+        conditions.append("status = ?")
+        params.append(status)
+
+    sql = "SELECT * FROM compute_units"
+
+    if conditions:
+        sql += " WHERE " + " AND ".join(conditions)
+
+    if limit:
+        sql += f" LIMIT {limit}"
+
+    with sqlite3.connect(SQLITE) as conn:
+        conn.row_factory = sqlite3.Row  # enables dict-like access
+
+        cur = conn.cursor()
+
+        rs = cur.execute(sql, params).fetchall()
+
+        response = []
+
+        for row in rs:
+            data = dict(row)
+
+            # Parse tags JSON
+            data["tags"] = json.loads(data["tags"]) if data.get("tags") else None
+
+            # Parse started_at timestamp
+            if data.get("started_at"):
+                data["started_at"] = dt.datetime.fromisoformat(data["started_at"])
+            else:
+                data["started_at"] = None
+
+            response.append(ComputeUnit(**data))
+
+        return response
+
+
 # Configure the templates
 templates = Jinja2Templates(directory="kloigos/templates")
 
@@ -226,16 +282,13 @@ templates = Jinja2Templates(directory="kloigos/templates")
 @app.get("/", response_class=HTMLResponse)
 async def inventory_dashboard(request: Request):
 
-    # 1. Load the current inventory data from the YAML file
-    with open(INVENTORY_FILE, "r") as f:
-        inventory = yaml.safe_load(f)
+    inventory = {x.compute_id: x.model_dump() for x in get_compute_units()}
 
-    for compute_id, v in inventory.items():
+    for _, v in inventory.items():
         cpu_list = cpu_range_to_list(v.get("cpu_range"))
         start_port = CPU_PORTS_MAP.get(cpu_list.split(",")[-1])
 
         v["cpu_list"] = cpu_list
-        v["cpu_count"] = len(cpu_list.split(","))
         v["ports_range"] = f"{start_port}-{start_port+RANGE_SIZE}"
 
     # 2. Prepare the context dictionary to pass data to the HTML template
@@ -245,5 +298,4 @@ async def inventory_dashboard(request: Request):
         "hosts": inventory,
     }
 
-    # 3. Render the HTML page
     return templates.TemplateResponse("dashboard.html", context)
---
+++ kloigos/util.py
@@ -0,0 +1,106 @@
+import datetime as dt
+import json
+import os
+import shutil
+
+import ansible_runner
+import yaml
+
+
+class MyRunner:
+
+    data = {}
+
+    def my_status_handler(self, status, runner_config):
+        return
+
+    def my_event_handler(self, e):
+        task_type = ""
+        task_data = ""
+
+        if e["event"] in [
+            "verbose",
+            "playbook_on_start",
+            "playbook_on_no_hosts_matched",
+            "runner_on_skipped",
+            "runner_item_on_skipped",
+            "runner_item_on_ok",
+            "runner_on_start",
+            "runner_retry",
+            "playbook_on_include",
+        ]:
+            return
+
+        elif e["event"] == "runner_on_ok":
+            if e.get("event_data")["task"] == "Data":
+                self.data = e["event_data"]["res"]["msg"]
+            else:
+                return
+
+        elif e["event"] == "warning":
+            task_type = "WARNING"
+            task_data = e["stdout"]
+
+        elif e["event"] == "error":
+            task_type = "ERROR"
+            task_data = e["stdout"]
+
+        elif e["event"] == "playbook_on_play_start":
+            task_type = f"PLAY [{e['event_data']['play']}]"
+
+        elif e["event"] == "playbook_on_task_start":
+            task_type = f"{e['event_data']['task']}"
+
+        elif e["event"] == "runner_on_failed":
+            task_data = f"fatal: [{e['event_data']['host']}]\n{json.dumps(e['event_data']['res']['msg'])}"
+
+        elif e["event"] == "runner_item_on_failed":
+            task_data = f"fatal: [{e['event_data']['host']}]\n{e['event_data']['res']['stderr']}"
+
+        elif e["event"] == "playbook_on_stats":
+            task_type = "PLAY RECAP"
+            task_data = (
+                f"ok: {e['event_data']['ok']} \nfailures: {e['event_data']['failures']}"
+            )
+
+        else:
+            # new event not being catched
+            task_type = e["event"]
+            task_data = json.dumps(e)
+
+        print(
+            e["created"],
+            task_type,
+            task_data,
+        )
+
+    def launch_runner(
+        self, playbook_name: str, extra_vars: dict
+    ) -> tuple[str, dict, int]:
+
+        with open("resources/" + playbook_name, "r") as f:
+            playbook = yaml.safe_load(f.read())
+
+        # create a new working directory
+
+        job_id = int(dt.datetime.timestamp())
+        os.mkdir(path=f"/tmp/job-{job_id}")
+
+        # Execute the playbook
+        try:
+            runner = ansible_runner.run(
+                quiet=False,
+                verbosity=1,
+                playbook=playbook,
+                private_data_dir=f"/tmp/job-{self.job_id}",
+                extravars=extra_vars,
+                event_handler=self.my_event_handler,
+                status_handler=self.my_status_handler,
+            )
+        except Exception as e:
+            print(f"Error running playbook: {e}")
+
+        # rm -rf job-directory
+        shutil.rmtree(f"/tmp/job-{job_id}", ignore_errors=True)
+
+        return runner.status
